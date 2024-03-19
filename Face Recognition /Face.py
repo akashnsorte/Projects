7260e98fb84a510ab6f9ff1512db93c763e76b65
Akash Sorte <96072586+akashnsorte@users.noreply.github.com>
2024-03-19 10:13:56 +0530

    Update Face.py

--- Face Recognition /Face.py
+++ Face Recognition /Face.py
@@ -12,28 +12,21 @@ if not cap.isOpened():
     exit()
 
 while True:
-    # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Check if the frame was captured successfully
     if not ret:
         print("Error: Unable to capture frame.")
         break
 
     # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the frame
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Draw rectangles around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Display the resulting frame
     cv2.imshow('Face Detection', frame)
 
-    # Break the loop when 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
